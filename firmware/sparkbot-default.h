384544424587bfe584f77868db8d371e0ece269f
Nathan Robinson <nrobinson2000@me.com>
2015-04-28 21:14:35 +0100

    update

---
+++ firmware/sparkbot-default.h
@@ -0,0 +1,20 @@
+#ifndef sparkbot_default_h
+#define sparkbot_default_h
+
+void startup();
+void enableRightButton();
+void enableLeftButton();
+void switchLights();
+void red();
+void blue();
+void green();
+void syncLights();
+void syncServos();
+void moveNeck();
+void moveRight();
+void moveLet();
+
+
+
+
+#endif
