663341a8990507438af97adea78a63e464b95ea0
nrobinson2000 <nrobinson2000@me.com>
2015-04-30 08:04:01 +0100

    Update sparkbot-default.cpp

--- firmware/sparkbot-default.cpp
+++ firmware/sparkbot-default.cpp
@@ -6,37 +6,37 @@ Servo leftservo;
 Servo neckservo;
 
 // Servo angles
-int neckAngle = 0;
-int rightArmAngle = 0;
-int leftArmAngle = 0;
+neckAngle = 0;
+rightArmAngle = 0;
+leftArmAngle = 0;
 
 // Buttons
-int rightbutton = D7;
-int leftbutton = A5;
+rightbutton = D7;
+leftbutton = A5;
 
 // LED Pins
-int redled = D4;
-int blueled = D5;
-int greenled = D6;
+redled = D4;
+blueled = D5;
+greenled = D6;
 
 // LED Booleans
-bool redledOn = false;
-bool blueledOn = false;
-bool greenledOn = false;
+redledOn = false;
+blueledOn = false;
+greenledOn = false;
 
 // Sensor Pins
-int temperature = D3;
-int microphone = A1;
+temperature = D3;
+microphone = A1;
 
 // Other Pins
-int buzzer = A0;
+buzzer = A0;
 
 // I/O pins
-int io1 = A2;
-int io2 = A3;
-int io3 = A4;
+io1 = A2;
+io2 = A3;
+io3 = A4;
 
-int choice = 0; // Used for random tests
+choice = 0; // Used for random tests
 
 void startup()
 {
