9adc21c4698a9f566eeb46d81c673922344c5dc0
Nathan Robinson <nrobinson2000@me.com>
2015-05-13 17:29:13 +0100

    come on

--- firmware/sparkbot-default.cpp
+++ firmware/sparkbot-default.cpp
@@ -72,9 +72,9 @@ void sparkbot::startup()
   Spark.variable("rightArm", &rightArmAngle, INT);
   Spark.variable("leftArm", &leftArmAngle, INT);
 
-  Spark.function("moveNeck", (voidFuncPtr)&sparkbot::moveNeckCloud);
-  Spark.function("moveRight", (voidFuncPtr)&sparkbot::moveRightCloud);
-  Spark.function("moveLeft", (voidFuncPtr)&sparkbot::moveLeftCloud);
+  Spark.function("moveNeck", int (*)(String)&sparkbot::moveNeckCloud);
+  Spark.function("moveRight", int (*)(String)&sparkbot::moveRightCloud);
+  Spark.function("moveLeft", int (*)(String)&sparkbot::moveLeftCloud);
 }
 
 
