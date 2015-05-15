d4331f641b7462be6823b4becab1451726ef0c6f
Nathan Robinson <nrobinson2000@me.com>
2015-05-15 20:44:42 +0100

    fixed semicolons

--- firmware/example1.ino
+++ firmware/example1.ino
@@ -34,55 +34,79 @@ void loop()
 
 }
 
-int tinkerDigitalRead(String pin) {
+int tinkerDigitalRead(String pin)
+{
     int pinNumber = pin.charAt(1) - '0';
     if (pinNumber< 0 || pinNumber >7) return -1;
-    if(pin.startsWith("D")) {
+    if(pin.startsWith("D"))
+    {
         pinMode(pinNumber, INPUT_PULLDOWN);
-        return digitalRead(pinNumber);}
-    else if (pin.startsWith("A")){
+        return digitalRead(pinNumber);
+        }
+    else if (pin.startsWith("A"))
+    {
         pinMode(pinNumber+10, INPUT_PULLDOWN);
-        return digitalRead(pinNumber+10);}
-    return -2;}
+        return digitalRead(pinNumber+10);
+        }
+    return -2;
+}
 
-int tinkerDigitalWrite(String command){
+int tinkerDigitalWrite(String command)
+{
     bool value = 0;
     int pinNumber = command.charAt(1) - '0';
     if (pinNumber< 0 || pinNumber >7) return -1;
     if(command.substring(3,7) == "HIGH") value = 1;
     else if(command.substring(3,6) == "LOW") value = 0;
     else return -2;
-    if(command.startsWith("D")){
+    if(command.startsWith("D"))
+    {
         pinMode(pinNumber, OUTPUT);
         digitalWrite(pinNumber, value);
-        return 1;}
-    else if(command.startsWith("A")){
+        return 1;
+    }
+    else if(command.startsWith("A"))
+    {
         pinMode(pinNumber+10, OUTPUT);
         digitalWrite(pinNumber+10, value);
-        return 1;}
-    else return -3;}
+        return 1;
+    }
+    else return -3;
+}
 
-int tinkerAnalogRead(String pin){
+int tinkerAnalogRead(String pin)
+{
     int pinNumber = pin.charAt(1) - '0';
     if (pinNumber< 0 || pinNumber >7) return -1;
-    if(pin.startsWith("D")){
+    if(pin.startsWith("D"))
+    {
         pinMode(pinNumber, INPUT);
-        return analogRead(pinNumber);}
-    else if (pin.startsWith("A")){
+        return analogRead(pinNumber);
+    }
+    else if (pin.startsWith("A"))
+    {
         pinMode(pinNumber+10, INPUT);
-        return analogRead(pinNumber+10);}
-    return -2;}
+        return analogRead(pinNumber+10);
+    }
+    return -2;
+  }
 
-int tinkerAnalogWrite(String command){
+int tinkerAnalogWrite(String command)
+{
     int pinNumber = command.charAt(1) - '0';
     if (pinNumber< 0 || pinNumber >7) return -1;
     String value = command.substring(3);
-    if(command.startsWith("D")){
+    if(command.startsWith("D"))
+    {
         pinMode(pinNumber, OUTPUT);
         analogWrite(pinNumber, value.toInt());
-        return 1;}
-    else if(command.startsWith("A")){
+        return 1;
+        }
+    else if(command.startsWith("A"))
+    {
         pinMode(pinNumber+10, OUTPUT);
         analogWrite(pinNumber+10, value.toInt());
-        return 1;}
-    else return -2;}
+        return 1;
+    }
+    else return -2;
+  }
