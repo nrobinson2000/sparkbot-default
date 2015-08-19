bb0fef27a8efc2dce97cb2e3f32c1284854e5832
nrobinson2000 <nrobinson2000@github.com>
2015-08-19 22:04:24 +0100

    update

---
+++ travis-helper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/expect
+set timeout 20
+
+spawn "particle cloud login"
+expect "? Please enter your email address: "
+send $EMAIL
+
+expect "? Please enter your password: "
+send $PASS
+
+spawn "particle compile photon firmware/"
