df5858a9a8d4d81ced74e7d0fa076f557e7273c5
nrobinson2000 <nrobinson2000@github.com>
2015-08-20 11:25:44 +0100

    fix

--- travis-helper.sh
+++ travis-helper.sh
@@ -4,9 +4,9 @@ set timeout 20
 
 spawn /home/travis/.nvm/v0.10.36/bin/particle cloud login
 expect "? Please enter your email address: "
-send "$(echo $email)"
+send "$(echo ${email})"
 
 expect "? Please enter your password: "
-send "$(echo $pass)""
+send "$(echo ${pass})""
 
 spawn /home/travis/.nvm/v0.10.36/bin/particle compile photon firmware/
